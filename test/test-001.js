6e05fbd85fa3b3782f27df7da33eec13cb8f573d
llimacruz <leonardo.cruz@outlook.com>
2018-07-08 21:01:32 -0300

    Initial commit with problem 1 solved

---
+++ test/test-001.js
@@ -0,0 +1,12 @@
+const assert = require('chai').assert;
+const findSum = require('../lib/problem-001.js');
+
+it('Menor que 10', () => {
+  const sum = findSum(10);
+  assert.equal(sum, 23);
+});
+
+it('Menor que 1000', () => {
+  const sum = findSum(1000);
+  assert.equal(sum, 23);
+});
